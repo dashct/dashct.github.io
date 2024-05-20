770e8a1e33a7d88584f7416146e37bb2e0387f7b
dashct <56509237+dashct@users.noreply.github.com>
2024-05-20 20:26:29 +0100

    partners list

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+var list = document.getElementById('enemies');
+
+function addEnemy() {
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode("harry jenko"));
+    list.appendChild(entry);
+}
+
 function joinClub() {
     alert("you have joined the DCT fanclub 🎉");
 }
